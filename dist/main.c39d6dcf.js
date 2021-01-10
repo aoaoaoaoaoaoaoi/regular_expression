9f1058c37b971d4e992635f5d1dc642f518088cc
unknown <gummi.bear456aaaaa@gmail.com>
2021-01-11 01:25:35 +0900

    ensure that files are modified in the build

--- dist/main.c39d6dcf.js
+++ dist/main.c39d6dcf.js
@@ -9097,7 +9097,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54266" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56101" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
